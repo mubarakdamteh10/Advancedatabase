562ffa7f1ac4993904c5dbaa36622d7da94ea867
iwnlyfe <iwnlyfe@hotmail.com>
2021-09-17 09:11:32 +0700

    upload file hero on frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,14 @@ import React from "react"
 import Breadcrumb from "../component/Breadcrump"
 import MightYouLike from "../component/MightYouLike"
 import ShowAnime from "../component/ShowAnime"
-
+import Hero from "../component/Hero"
 
 
 export default class Home extends React.Component{
     render(){
         return(
             <>
+            <Hero />
             {/* <Breadcrumb/> */}
             <ShowAnime/>
             {/* <MightYouLike /> */}
